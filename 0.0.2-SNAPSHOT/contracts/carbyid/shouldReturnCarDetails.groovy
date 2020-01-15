8d3795a9f1f40a723b62a0f6e4b2b837320f9716
gjimenez-ruiz <gjimenez-ruiz.ctr@kr.af.mil>
2020-01-15 13:12:32 -0500

    Initial commit contracts

---
+++ 0.0.2-SNAPSHOT/contracts/carbyid/shouldReturnCarDetails.groovy
@@ -0,0 +1,37 @@
+package contracts.amubyid
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+
+    name("Should return single car" )
+    description()
+
+    request {
+
+        method GET()
+
+        url("/api/v1/car/b55dc50f-600f-4cbc-9b49-fed186cf6eb2" ) {
+
+            headers {
+                header( authorization(), $( c('Bearer dGVzdA==' ), p( execute( 'authToken()' ) ) ) )
+            }
+
+        }
+
+    }
+
+    response {
+
+        status OK()
+
+        body( file("carDetails.json" ) )
+
+        headers {
+            contentType( applicationJson() )
+        }
+
+    }
+
+}
+
